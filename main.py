640fa4c054206ab4c1f994afdd7b43f43a0e2815
M <mail>
2021-04-14 17:59:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+# #zad1
+# czworki = np.arange(4, 84, 4)
+# print(czworki)
+
+
+
+# #zad2
+# lista = np.arange(1, 2, 0.1 , dtype="float")
+# print(lista)
+# lista2 = lista.astype("int64")
+# print(lista2)
+
+
+
+# #zad3
+# def funkcja(n):
+#     tablica = np.ndarray(shape=(n,n), dtype="int32", buffer=np.arange(1, n*n+1, 1))
+#     return tablica
+#
+# print(funkcja(5))
+
+
+# #zad5
+# def funkcja3(dlugosc):
+#     macierz = np.diag([a for a in range(dlugosc)])
+#     macierz = np.flip(macierz)
+#     return macierz
+#
+# print(funkcja3(5))
+
+
+
+# #zad9
+# a = 3
+# n = 3
+# a_k = a + 24*n
+# macierz = np.linspace(a, a_k, 25)
+# macierz = macierz.reshape((5, 5))
+# print(macierz)
+
